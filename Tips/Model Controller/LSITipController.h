5a2622278d853eaf117baf58a70b16bb5add1958
Craig Swanson <cswanson02@me.com>
2020-04-09 21:16:21 -0500

    Fleshed out public interface for the model controller. Used 'a' in front of the tip argument as a common naming choice.

--- Tips/Model Controller/LSITipController.h
+++ Tips/Model Controller/LSITipController.h
@@ -15,6 +15,11 @@ NS_ASSUME_NONNULL_BEGIN
 @interface LSITipController : NSObject
 
 @property (nonatomic, readonly, copy) NSArray<LSITip *> *tips;
+@property (nonatomic, readonly) NSUInteger tipCount;
+
+- (LSITip *)tipAtIndex:(NSUInteger)index;
+
+- (void)addTip:(LSITip *)aTip;
 
 @end
 
