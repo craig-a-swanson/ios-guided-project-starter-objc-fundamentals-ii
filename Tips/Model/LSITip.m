dd698d1e77d62eddc9bad18d53b38630e41466ca
Craig Swanson <cswanson02@me.com>
2020-04-09 20:46:11 -0500

    Added model file and updated the class prefix for all files in the project.

---
+++ Tips/Model/LSITip.m
@@ -0,0 +1,13 @@
+//
+//  LSITip.m
+//  Tips
+//
+//  Created by Craig Swanson on 4/9/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSITip.h"
+
+@implementation LSITip
+
+@end
